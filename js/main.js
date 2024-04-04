6537d87f2777475ea7434246ea1af372b7c27991
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-04-04 14:29:19 +0100

    Commit 2 Main.js
    Hover Feature for Supercars Page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-// Toggle navigation visibility
+// Toggle Navigation Visibility
 document.querySelector('.menu-toggle').addEventListener('click', function() {
   document.querySelector('header').classList.toggle('nav-open');
 });
+
+// Supercars Hover Mapping Content
+function showInfo(info) {
+  document.getElementById('hover-text').textContent = info;
+  document.getElementById('hover-text').style.display = 'block';
+}
+
+function hideInfo() {
+  document.getElementById('hover-text').textContent = '';
+  document.getElementById('hover-text').style.display = 'none';
+}
